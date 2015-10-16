1ea0c4568f702987f024bd6a702c6b2fdfde1891
Daniel Schosser <daniel.schosser@gmail.com>
2015-10-16 15:50:30 +0200

    Cleanup Xing Crawler

--- app/Libraries/Crawler/XingCrawler.php
+++ app/Libraries/Crawler/XingCrawler.php
@@ -6,10 +6,61 @@
  * Time: 14:34
  */
 
-namespace App\Crawler;
+namespace App\Libraries\Crawler;
 
+use App\Models\Person;
+use Sunra\PhpSimple\HtmlDomParser;
 
 class XingCrawler
 {
+    public static function crawl($url) {
+        $findings = [];
+
+        $agent= 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36';
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_VERBOSE, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, $agent);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $html = curl_exec($curl);
+
+        if (curl_error($curl))
+            die(curl_error($curl));
+
+        // Get the status code
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        curl_close($curl);
+
+        return XingCrawler::analyze($html);
+    }
+
+    private static function analyze($html) {
+        $dom = HtmlDomParser::str_get_html( $html );
+        $person = new Person();
+
+        $fullName = $dom->find('h1.username', 0)->plaintext;
+        $explodedName = explode(' ', $fullName);
+        $firstName = $explodedName[0];
+        $lastName = $explodedName[1];
+
+        $jobTitle = $dom->find('.job-info .job-title', 0)->plaintext;
+//        $companyName = $explodedTitle[1];
+//
+//        $location = $dom->find('#location .locality', 0)->plaintext;
+//        $industry = $dom->find('#location .industry', 0)->plaintext;
+//        $website = $dom->find('#overview-summary-websites a', 0)->href;
+
+        $person->addAttribute("first-name", $firstName)
+            ->addAttribute('last-name', $lastName)
+            ->addAttribute('job-title', $jobTitle);
+//            ->addAttribute('company', $companyName)
+//            ->addAttribute('location', $location)
+//            ->addAttribute('industry', $industry)
+//            ->addAttribute('website', $website);
+        return $person;
+    }
 
 }
--- app/Libraries/Import/Import.php
+++ app/Libraries/Import/Import.php
@@ -6,10 +6,27 @@
  * Time: 14:12
  */
 
-namespace App\Libraries;
+namespace App\Libraries\Import;
 
 
-class Import
+abstract class Import
 {
+
+    /**
+     * Import a file containing the results from a specific tool.
+     * The file then gets parsed by the analyze method and returns the findings
+     *
+     * @param $file Exported results from a tool (json|xml)
+     * @return mixed
+     */
+    abstract public function import($file);
+
+    /**
+     * Parses the imported file for its content and adds each finding to an array,
+     * which will be returned to the import method.
+     *
+     * @return mixed
+     */
+    abstract protected function analyze();
 
 }
