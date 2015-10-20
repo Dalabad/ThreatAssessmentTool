e2a0a33bba698a9acc8fb94e9b79fe6306fb437b
Daniel Schosser <daniel.schosser@gmail.com>
2015-10-20 12:44:10 +0200

    Update Crawler

--- app/Libraries/Crawler/LinkedInCrawler.php
+++ app/Libraries/Crawler/LinkedInCrawler.php
@@ -15,8 +15,6 @@ use Sunra\PhpSimple\HtmlDomParser;
 class LinkedInCrawler
 {
     public static function crawl($url) {
-        $findings = [];
-
         $agent= 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36';
 
         $curl = curl_init($url);
