513b456e8c074c7f73e84d462c59ce5f828d1a98
Daniel Schosser <daniel.schosser@gmail.com>
2015-11-12 14:38:32 +0100

    Implemented Maltego Importer

--- app/Libraries/Importer/MaltegoImporter.php
+++ app/Libraries/Importer/MaltegoImporter.php
@@ -9,13 +9,14 @@
 namespace App\Libraries\Import;
 
 
+use App\Libraries\Importer\Importer;
 use Nathanmac\Utilities\Parser\Facades\Parser;
 
 class MaltegoImporter extends Importer
 {
 
-    public $findings;
-    private $importedFile;
+    public $_findings;
+    private $_importedFile;
 
     public function import($file)
     {
@@ -24,33 +25,47 @@ class MaltegoImporter extends Importer
         $this->_findings['emails']    = [];
         $this->_findings['locations'] = [];
 
-        $this->importedFile = file_get_contents($file);
+        $fileContent = file_get_contents($file);
+        $fileContent = str_replace(
+            [
+                'version="1.1"', // Change Version to 1.0
+                ' xmlns:mtg="http://maltego.paterva.com/xml/mtgx"', // Remove Schema
+                'mtg:' // Remove Namespace
+            ],
+            [
+                'version="1.0"',
+                '',
+                ''
+            ],
+            $fileContent
+        );
+        $this->_importedFile = Parser::xml($fileContent);
 
         $this->analyze();
 
-        return $this->findings;
+        return $this->_findings;
     }
 
     protected function analyze()
     {
-        $array = Parser::xml($this->importedFile)->get('graph');
-        //die('<pre>'.var_dump($array));
+        foreach($this->_importedFile['graph']['node'] as $node) {
+            $entity = $node['data'][0]['MaltegoEntity'];
 
-        foreach($array['node'] as $node) {
-            $entity = $node['data']['mtg:MaltegoEntity'];
-
-            switch($entity['type']) {
+            switch($entity['@attributes']['type']) {
                 case "maltego.DNSName":
+                    $this->_findings['websites'][] = $entity['Properties']['Property']['Value'];
+                    break;
+                case "maltego.Website":
+                    $this->_findings['websites'][] = $entity['Properties']['Property'][0]['Value'];
                     break;
                 case "maltego.Domain":
+                    $this->_findings['websites'][] = $entity['Properties']['Property'][0]['Value'];
                     break;
                 case "maltego.EmailAddress":
-                    $email = "";
+                    $email = $entity['Properties']['Property'][0]['Value'];
                     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
+                        $this->_findings['emails'][] = $email;
                     }
-                    break;
-                case "maltego.link.transform-link":
                     break;
             }
         }
